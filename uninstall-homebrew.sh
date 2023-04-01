0f975e366ccc190620cd64d5d0024d1cb000a9e7
implicitfield <114500360+implicitfield@users.noreply.github.com>
2023-04-01 22:17:18 +0400

    Reinstall CMake

--- uninstall-homebrew.sh
+++ uninstall-homebrew.sh
@@ -10,3 +10,11 @@ sudo ./uninstall.sh --force
 sudo rm -rf /usr/local/Homebrew
 sudo rm -rf /usr/local/Caskroom
 sudo rm -rf /usr/local/bin/brew
+
+# Since we just removed CMake, let's add it back.
+CMAKE_RELEASE="$(cat cmake-release.txt)"
+curl -O -L https://github.com/Kitware/CMake/releases/download/v"$CMAKE_RELEASE"/cmake-"$CMAKE_RELEASE"-macos10.10-universal.tar.gz
+[ "$(shasum -a 512 cmake-$CMAKE_RELEASE-macos10.10-universal.tar.gz)" != "$(cat cmake-checksum.txt)" ] && exit 1
+tar -xf cmake-"$CMAKE_RELEASE"-macos10.10-universal.tar.gz
+sudo cp -r cmake-"$CMAKE_RELEASE"-macos10.10-universal/CMake.app/Contents/bin/* /usr/local/bin/
+sudo cp -r cmake-"$CMAKE_RELEASE"-macos10.10-universal/CMake.app/Contents/share/* /usr/local/share/
