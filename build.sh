61dc6d4bdadf98c4c9f9ebeb158060779d00cfac
implicitfield <114500360+implicitfield@users.noreply.github.com>
2023-04-02 15:04:04 +0400

    Another revision bump
    Changes:
    - Do not emebed git revision metadata in build artifacts
    - Improve the formatting of release notes

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ fi
 cp final/llvm-project/llvm/utils/release/test-release.sh .
 patch -p1 < 0001-skip-tests.patch
 patch -p1 < 0002-support-partitioned-builds.patch
-./test-release.sh -release "$LLVM_RELEASE" -final -triple x86-64-apple-darwin21.0 -no-checkout -no-clang-tools -no-test-suite -no-openmp -no-polly -no-mlir -no-flang -use-gzip -no-compare-files -"$1"
+./test-release.sh -release "$LLVM_RELEASE" -final -triple x86-64-apple-darwin21.0 -no-checkout -no-clang-tools -no-test-suite -no-openmp -no-polly -no-mlir -no-flang -use-gzip -no-compare-files -configure-flags -DLLVM_APPEND_VC_REV=OFF -"$1"
 if [ "$1" != "stage3" ]; then
     tar -cf build_directory.tar final
     exit 0
@@ -15,6 +15,8 @@ _release_tag_version="$LLVM_RELEASE"
 [ "$(cat revision.txt)" -ne 0 ] && _release_tag_version="$_release_tag_version"-"$(cat revision.txt)"
 echo "file_name=clang+llvm-$LLVM_RELEASE-x86-64-apple-darwin21.0.tar.gz" >> $GITHUB_OUTPUT
 echo "release_tag_version=$_release_tag_version" >> $GITHUB_OUTPUT
-shasum -a 512 final/clang+llvm-"$LLVM_RELEASE"-x86-64-apple-darwin21.0.tar.gz | sed 's,final/,,' > github_release_text.txt
+printf 'SHA512 checksum:\n<code>' > github_release_text.md
+printf "$(shasum -a 512 final/clang+llvm-"$LLVM_RELEASE"-x86-64-apple-darwin21.0.tar.gz | sed 's,final/,,' | sed 's, ,\&nbsp;,g')" >> github_release_text.md
+printf '</code>\n' >> github_release_text.md
 mkdir output
 mv final/clang+llvm-"$LLVM_RELEASE"-x86-64-apple-darwin21.0.tar.gz output/
