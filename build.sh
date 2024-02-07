9ca6f81249e807fee032babba5e623700427df69
implicitfield <114500360+implicitfield@users.noreply.github.com>
2024-02-07 15:27:23 +0400

    (Experimental): Update to LLVM 17.0.6 + use arm64 runners

--- build.sh
+++ build.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 LLVM_RELEASE="$(cat llvm-release.txt)"
-if [ "$1" != "stage1" ]; then
+if [ "$2" != "stage1" ]; then
     tar -xf build_directory.tar
 fi
 cp final/llvm-project/llvm/utils/release/test-release.sh .
 patch -p1 < 0001-skip-tests.patch
 patch -p1 < 0002-support-partitioned-builds.patch
 patch -p1 < 0003-use-built-in-xz-compression.patch
-./test-release.sh -release "$LLVM_RELEASE" -final -triple x86-64-apple-darwin21.0 -no-checkout -no-clang-tools -no-test-suite -no-openmp -no-polly -no-mlir -no-flang -no-compare-files -configure-flags -DLLVM_APPEND_VC_REV=OFF -"$1"
-if [ "$1" != "stage3" ]; then
+./test-release.sh -release "$LLVM_RELEASE" -final -triple "$1"-apple-darwin21.0 -no-checkout -no-clang-tools -no-test-suite -no-openmp -no-polly -no-mlir -no-flang -no-compare-files -configure-flags -DLLVM_APPEND_VC_REV=OFF -"$2"
+if [ "$2" != "stage3" ]; then
     tar -cf build_directory.tar final
     exit 0
 fi
-_release_tag_version="$LLVM_RELEASE"
+_release_tag_version="$LLVM_RELEASE"-"$1"
 [ "$(cat revision.txt)" -ne 0 ] && _release_tag_version="$_release_tag_version"-"$(cat revision.txt)"
 echo "file_name=clang+llvm-$LLVM_RELEASE-x86-64-apple-darwin21.0.tar.xz" >> $GITHUB_OUTPUT
 echo "release_tag_version=$_release_tag_version" >> $GITHUB_OUTPUT
