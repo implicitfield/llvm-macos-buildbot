d2af494b7347508c9dd3e157a00c5ddb5f24eddd
implicitfield <114500360+implicitfield@users.noreply.github.com>
2025-08-29 01:25:16 +0300

    Import d64802d from upstream
    There's a discussion about this on LLVM's issue tracker (#155532). In
    case this doesn't work, we'll need to either disable lldb or wait for a
    fix to land.

--- initialize.sh
+++ initialize.sh
@@ -6,3 +6,4 @@ mkdir final
 tar -xf llvm-project-"$LLVM_RELEASE".src.tar.xz
 mv llvm-project-"$LLVM_RELEASE".src final/llvm-project
 patch -p1 -R -d final/llvm-project < 4dec62f4d4a0a496a8067e283bf66897fbf6e598.patch
+patch -p1 -d final/llvm-project < d64802d6d96ec5aff3739ce34f8143b935921809.patch
