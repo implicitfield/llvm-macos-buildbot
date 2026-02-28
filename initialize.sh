b4b88dc974e633f0503a672c1d946f21a3a27636
implicitfield <114500360+implicitfield@users.noreply.github.com>
2026-02-28 20:21:12 +0200

    Update to LLVM 22.1.0
    This updates the target triple to match the OS that these binaries are
    built on. This is a breaking change for any scripts using the old
    triple.
    
    This also removes an obsolete cherry-pick and now fixes #127764 with a
    cherry-picked commit.

--- initialize.sh
+++ initialize.sh
@@ -6,7 +6,5 @@ LLVM_RELEASE="$(cat llvm-release.txt)"
 mkdir final
 tar -xf llvm-project-"$LLVM_RELEASE".src.tar.xz
 mv llvm-project-"$LLVM_RELEASE".src final/llvm-project
-# llvm/llvm-project/issues/155532
-patch -p1 -d final/llvm-project < d64802d6d96ec5aff3739ce34f8143b935921809.patch
 # llvm/llvm-project/issues/127764
-patch -p1 -d final/llvm-project < add-rtsan-libcxx-dependency.patch
+patch -p1 -d final/llvm-project < 46de3200388c300ee871f5b4941dff0d97ba9480.patch
